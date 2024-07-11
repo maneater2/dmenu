29211eebec3a39105303fd174f4f4f5402cd06f8
Light <114833619+maneater2@users.noreply.github.com>
2024-07-11 19:43:38 +0300

    Update config.mk

--- config.mk
+++ config.mk
@@ -15,8 +15,6 @@ XINERAMAFLAGS = -DXINERAMA
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
 FREETYPEINC = /usr/include/freetype2
-# OpenBSD (uncomment)
-#FREETYPEINC = $(X11INC)/freetype2
 
 # includes and libs
 INCS = -I$(X11INC) -I$(FREETYPEINC)
